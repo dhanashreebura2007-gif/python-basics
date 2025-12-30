fc300d8e5ae0b452948f77fd9f79074088c1793b
Dhanashree Suresh Bura <dhanashreebura2007@gmail.com>
2025-12-30 16:58:10 +0530

    Day 1: Added basic Python programs

---
+++ day1_basics.py
@@ -0,0 +1,22 @@
+# Day 1 - Python Basics
+# Name: Dhanashree Bura
+# Branch: CSE (AI & ML)
+# Topic: Basic Python Programs
+
+# Program 1: Hello World
+print("Hello World")
+
+# Program 2: Print Name
+print("My name is Dhanashree")
+
+# Program 3: Addition of two numbers
+a = 10
+b = 5
+print("Sum =", a + b)
+
+# Program 4: Check Even or Odd
+num = 7
+if num % 2 == 0:
+    print("Even Number")
+else:
+    print("Odd Number")
