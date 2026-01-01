9db1369255e56d99f888a3be780f8d679979ed22
Dhanashree Suresh Bura <dhanashreebura2007@gmail.com>
2026-01-01 15:37:16 +0530

    Day-2 input and if-else programs

--- day2_if_else.py
+++ day2_if_else.py
@@ -1,11 +1,11 @@
-# PROGRAM 1 : EVEN OR ODD  
+# PROGRAM 1: EVEN OR ODD  
 num = int(input("Enter a number: "))
 if num % 2 == 0:
   print("Even number")
 else:
   print("Odd number")
 
-# PROGRAM 2 : POSITIVE / NEGATIVE / ZERO
+# PROGRAM 2: POSITIVE / NEGATIVE / ZERO
 num = int(input("Enter a number: "))
 if num > 0:
   print("Positive")
@@ -14,7 +14,7 @@ elif num < 0:
 else:
   print("Zero")
 
-# PROGRAM 3 : LARGEST OF TWO NUMBER
+# PROGRAM 3: LARGEST OF TWO NUMBER
 a = int(input("Enter first number: "))
 b = int(input("Enter second number: "))
 if a > b:
@@ -22,7 +22,7 @@ if a > b:
 else:
     print("b is greater")
 
-# PROGRAM 4 : PASS OR FAIL 
+# PROGRAM 4: PASS OR FAIL 
 marks = int(input("Enter marks: "))
 if marks >= 40:
     print("Pass")
